e34af56e938236dfbbd55ab65e0e0b84f9b14d1e
Hikmet Mete Çelik <127776407+HMeteCelik@users.noreply.github.com>
2026-06-16 20:24:10 +0300

    edit

--- llm.py
+++ llm.py
@@ -3,7 +3,7 @@ from langchain_ollama import OllamaEmbeddings
 
 llm = ChatOllama(
     model="qwen3:4b-instruct-2507-q4_K_M",
-    temperature=1,
+    temperature=0.5,
     num_ctx=8192,
 )
 embeddings = OllamaEmbeddings(model="bge-m3:567m")
